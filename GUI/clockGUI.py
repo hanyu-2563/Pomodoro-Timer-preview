145a84587249f5abd1338d74efd04eb588e27f9d
Taedigrade <61576316+hanyu-2563@users.noreply.github.com>
2023-04-29 13:56:35 +0800

    Add files via upload

--- GUI/clockGUI.py
+++ GUI/clockGUI.py
@@ -9,3 +9,14 @@ cycles = 0
 # 窗口设置
 window = tk.Tk() 
 window.title('番茄钟')
+
+# 计时器
+def work_timer(): 
+    work_sec = work_mins * 60
+    for sec in range(work_sec, 0, -1): 
+        mins, secs = divmod(sec, 60) 
+        time_format = '{:02d}:{:02d}'.format(mins, secs)  
+        work_timer_label['text'] = time_format  # 更新时间显示
+        window.update()      # 更新GUI
+        time.sleep(1)
+    start_short_break()      # 短休息
